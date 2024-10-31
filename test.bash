6e979e31115192d589ec39f6d02f3aa1e8d83b81
Junya Wada <s23c1148re@s.chibakoudai.jp>
2024-10-31 12:41:33 +0900

    test

--- test.bash
+++ test.bash
@@ -9,7 +9,19 @@ ng () {
 
 res=0
 
+### 正常な出力 ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] ||  ng "$LINENO"
-[ "${res}" = 0 ] && echo OK 
+
+### 変な出力 ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] 	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] 	  || ng "$LINENO"
+[ "${out}" = "" ] 	  || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+
 exit $res
