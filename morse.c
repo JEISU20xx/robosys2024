475e0834233a922ff839937a689e0994365657d9
Junya Wada <s23c1148re@s.chibakoudai.jp>
2024-11-26 18:35:55 +0900

    アルファベットのモールス信号変換を可能にした

--- morse.c
+++ morse.c
@@ -13,9 +13,35 @@ int main(){
         while((c = getchar()) != '\n'){
             if(isalpha(c)){
                 c = tolower(c);
-                printf("%c",c);
+                
+                if(c == 'a') printf("・ー");
+                else if(c == 'b') printf("ー・・・");
+                else if(c == 'c') printf("ー・ー・");
+                else if(c == 'd') printf("ー・・");
+                else if(c == 'e') printf("・");
+                else if(c == 'f') printf("・・ー・");
+                else if(c == 'g') printf("ーー・");
+                else if(c == 'h') printf("・・・・");
+                else if(c == 'i') printf("・・");
+                else if(c == 'j') printf("・ーーー");
+                else if(c == 'k') printf("ー・ー");
+                else if(c == 'l') printf("・ー・・");
+                else if(c == 'm') printf("ーー");
+                else if(c == 'n') printf("ー・");
+                else if(c == 'o') printf("ーーー");
+                else if(c == 'p') printf("・ーー・");
+                else if(c == 'r') printf("・ー・");
+                else if(c == 's') printf("・・・");
+                else if(c == 't') printf("ー");
+                else if(c == 'u') printf("・・ー");
+                else if(c == 'v') printf("・・・ー");
+                else if(c == 'w') printf("・ーー");
+                else if(c == 'x') printf("ー・・ー");
+                else if(c == 'y') printf("ー・ーー");
+                else if(c == 'z') printf("ーー・・");
             }
-            if(isdigit(c) || c == '.'){
+
+            else if(isdigit(c) || c == '.'){
                 printf("%c",c);
             }
         }
